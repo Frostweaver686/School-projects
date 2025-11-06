04ad2bb0edbc90d0ea9a96c474a556dc3e6a323c
Frostweaver686 <105998983+Frostweaver686@users.noreply.github.com>
2025-11-06 13:53:42 -0500

    Add files via upload

---
+++ projet C++ Validation des formats/validationFormat.h
@@ -0,0 +1,21 @@
+
+/*
+ * File:   validationFormat.h
+ * Author: etudiant
+ *
+ * Created on 20 mai 2022, 16:36
+ */
+
+#ifndef VALIDATIONFORMAT_H
+#define VALIDATIONFORMAT_H
+#include <string>
+#include <cstdlib>
+
+bool validerFormatNom (const std::string& p_nom);
+bool validerCodeIssn (const std::string& p_issn);
+bool validerCodeIsbn (const std::string& p_isbn);
+
+
+
+#endif /* VALIDATIONFORMAT_H */
+
